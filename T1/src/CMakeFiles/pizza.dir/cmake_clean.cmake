69d0479f1556d87094933f74848edf67d610c34b
AleGabriela15 <alexandra.g.fratila@gmail.com>
2023-11-03 15:54:30 +0200

    Tema 1 modificata

---
+++ T1/src/CMakeFiles/pizza.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pizza.dir/main.cpp.o"
+  "CMakeFiles/pizza.dir/main.cpp.o.d"
+  "CMakeFiles/pizza.dir/pizza.cpp.o"
+  "CMakeFiles/pizza.dir/pizza.cpp.o.d"
+  "pizza"
+  "pizza.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pizza.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
